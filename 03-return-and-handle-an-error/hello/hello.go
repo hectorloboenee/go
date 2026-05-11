04ed97c637ff0c230215733d5d8dcc8bd6cf56b9
Hector Lobo <hector.lobo.enee@gmail.com>
2026-05-11 17:37:48 -0600

    03 return and handle an error

---
+++ 03-return-and-handle-an-error/hello/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/greetings"
+)
+import "log"
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+}
